09ddb111233b98c97e8a37de674d32da3cbb48f1
Jeffrey Varner <jdv27@cornell.edu>
2022-12-17 11:26:00 -0500

    Create test_build_run_feedback_model.jl
    working on test to solve model

---
+++ test/test_build_run_feedback_model.jl
@@ -0,0 +1,14 @@
+# load package -
+using BSTModelKit
+
+function test_build_run_feedback_model()::Bool
+
+    # # set path to bst file -
+    # path_to_model_file = joinpath(pwd(),"data","Feedback.toml");
+
+    # # build -
+    # model_object = build(path_to_model_file);    
+
+    # return -
+    return true
+end
